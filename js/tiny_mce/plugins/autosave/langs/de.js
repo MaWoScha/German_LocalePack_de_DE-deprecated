33f67fe0c9928c7732d1ab6a98b79620157c2c13
MaWoScha <manfredwormschaefer@gmail.com>
2015-09-29 05:56:31 +0200

    Kleinkram zum 29. September 2015

--- js/tiny_mce/plugins/autosave/langs/de.js
+++ js/tiny_mce/plugins/autosave/langs/de.js
@@ -1,4 +1,1 @@
-tinyMCE.addI18n('de.autosave',{
-restore_content: "Wiederherstellung automatisch gespeicherter Inhalte",
-warning_message: "Wenn Sie die gespeicherten Inhalte wieder herstellen, werden Sie alle Inhalte, die sich derzeit in den Editor befinden, verlieren.\n\nSind Sie sicher, dass Sie die gespeicherte Inhalte wiederherstellen wollen?"
-});
+tinyMCE.addI18n('de.autosave',{restore_content: "Wiederherstellung automatisch gespeicherter Inhalte",warning_message: "Wenn Sie die gespeicherten Inhalte wiederherstellen, werden Sie alle Inhalte, die sich derzeit im Editor befinden, verlieren.\n\nSind Sie sicher, dass Sie die gespeicherte Inhalte wiederherstellen wollen?"});
